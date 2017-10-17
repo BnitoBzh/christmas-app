5c62659e508dcb3172ceae0f14279d3d731f36b7
Benoit Fievet <fievet.benoit@gmail.com>
2017-10-17 17:57:43 +0200

    list deletion

--- app/routes/lists/list.js
+++ app/routes/lists/list.js
@@ -3,5 +3,17 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model(params) {
     return this.get('store').findRecord('list', params.id, {reload: true, include: 'gifts'});
+  },
+
+  resetController(controller) {
+    this._super(...arguments);
+    controller.set('showDeleteConfirmationModal', false);
+  },
+
+  actions: {
+    deleteList(list) {
+      list.deleteRecord();
+      return list.save().then(() => this.transitionTo('lists'));
+    }
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -28,6 +28,7 @@ export default function() {
   this.get('/lists/:id');
   this.post('/lists');
   this.patch('/lists/:id');
+  this.del('/lists/:id');
 
   this.post('/gifts');
   this.patch('/gifts/:id');
