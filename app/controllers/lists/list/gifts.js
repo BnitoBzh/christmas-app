4e5adf8cab33130eb8639fdb3000755d73403e2d
Benoit Fievet <fievet.benoit@gmail.com>
2017-10-17 17:41:59 +0200

    add gifts sort/filter

--- app/controllers/lists/list/gifts.js
+++ app/controllers/lists/list/gifts.js
@@ -2,7 +2,35 @@ import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
 export default Controller.extend({
-  hasGifts: computed.gt('model.length', 0),
+  queryParams: ['filter', 'sortField', 'sortDirection'],
+  filter: 'all',
+  sortField: 'name',
+  sortDirection: 'asc',
+  sortProperties: computed('sortField', 'sortDirection', function() {
+    const { sortField, sortDirection } = this.getProperties('sortField', 'sortDirection');
+    let sortProperties = [`${sortField}:${sortDirection}`];
+    if (sortField === 'priority') {
+      sortProperties.push('name:asc');
+    }
+    return sortProperties;
+  }),
+
+  gifts: computed.filterBy('model', 'isNew', false),
+  hasGifts: computed.gt('gifts.length', 0),
+  filteredGifts: computed('gifts.@each.{isBought}', 'filter', function(){
+    const { filter, gifts } = this.getProperties('filter', 'gifts');
+    switch (filter) {
+      case 'bought':
+        return gifts.filterBy('isBought', true);
+      case 'available':
+        return gifts.filterBy('isBought', false);
+      default:
+        return gifts;
+    }
+  }),
+  sortedGifts: computed.sort('filteredGifts', 'sortProperties'),
+
+
   showEditForm: false,
   canAddGift: computed.or('hasGifts', 'showEditForm'),
 
